9bf03dd65fe9cdeb34461fb1a969fc4ca8a44a17
alex-kolovsky <kolovsky.dev@gmail.com>
2026-07-31 19:16:31 +0300

    feat(asm/boot.S): add and integrate boot assembly code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
 #![no_std]
 #![no_main]
 
-use core::{arch::asm, panic::PanicInfo};
+use core::{
+    arch::{asm, global_asm},
+    panic::PanicInfo,
+};
+
+global_asm!(include_str!("asm/boot.S"));
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {
+        unsafe {
+            asm!("wfi");
+        }
+    }
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn main() -> ! {
     loop {
         unsafe {
             asm!("wfi");
