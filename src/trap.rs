bd9ffbe80f6ba9158464e1a85e60dca15573c686
alex-kolovsky <kolovsky.dev@gmail.com>
2026-08-05 10:59:10 +0300

    feat(trap.rs): add VCPUContext struct to save CPU context on traps

--- src/trap.rs
+++ src/trap.rs
@@ -1,5 +1,49 @@
 const SXLEN: u8 = 64;
 
+struct VCPUContext {
+    // Host registers
+    host_pc: usize,
+    // PC
+    pc: usize,
+    // General purpose registers
+    ra: usize,
+    sp: usize,
+    gp: usize,
+    tp: usize,
+    t0: usize,
+    t1: usize,
+    t2: usize,
+    s0: usize,
+    s1: usize,
+    a0: usize,
+    a1: usize,
+    a2: usize,
+    a3: usize,
+    a4: usize,
+    a5: usize,
+    a6: usize,
+    a7: usize,
+    s2: usize,
+    s3: usize,
+    s4: usize,
+    s5: usize,
+    s6: usize,
+    s7: usize,
+    s8: usize,
+    s9: usize,
+    s10: usize,
+    s11: usize,
+    t3: usize,
+    t4: usize,
+    t5: usize,
+    t6: usize,
+    // CSRs
+    hstatus: usize,
+    hgatp: usize,
+    sstatus: usize,
+    sepc: usize,
+}
+
 macro_rules! read_csr {
     ($csr_name:expr) => {{
         let csr_value: u64;
