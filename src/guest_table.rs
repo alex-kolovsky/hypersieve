9bffd7ad0ac20fb9cf50e861d135cc22248bd059
alex-kolovsky <kolovsky.dev@gmail.com>
2026-08-07 16:00:09 +0300

    feat(guest_table.rs): add struct for storing Guest Page Table

--- src/guest_table.rs
+++ src/guest_table.rs
@@ -1,3 +1,5 @@
+use crate::println;
+
 pub const PTE_R: u64 = 1 << 1;
 pub const PTE_W: u64 = 1 << 2;
 pub const PTE_X: u64 = 1 << 3;
@@ -39,5 +41,44 @@ impl Table {
         // Each block is represented by the page table `level`.
         let index = (guest_paddr >> (12 + 9 * level)) & 0x1ff;
         &mut self.0[index as usize]
+    }
+}
+
+pub struct GuestPageTable {
+    table: *mut Table,
+}
+
+impl GuestPageTable {
+    pub fn new() -> Self {
+        Self {
+            table: Table::alloc(),
+        }
+    }
+
+    pub fn hgatp(&self) -> u64 {
+        // Page-based 50-bit virtual addressing (Sv48x4).
+        let mode_field = 9u64 << 60;
+
+        let table_ppn = self.table as u64 >> PPN_SHIFT;
+
+        mode_field | table_ppn
+    }
+
+    pub fn map(&self, guest_paddr: u64, host_paddr: u64, flags: u64) {
+        let mut table = unsafe { &mut *self.table };
+
+        for level in (1..=3).rev() {
+            let entry = table.entry_by_addr(guest_paddr, level);
+            if !entry.is_valid() {
+                let new_table_ptr = Table::alloc();
+                *entry = PageEntry::new(new_table_ptr as u64, PTE_V);
+            }
+            table = unsafe { &mut *(entry.paddr() as *mut Table) };
+        }
+
+        let entry = table.entry_by_addr(guest_paddr, 0);
+        println!("map: {:#x} -> {:#x}", guest_paddr, host_paddr);
+        assert!(!entry.is_valid(), "already mapped");
+        *entry = PageEntry::new(host_paddr, flags | PTE_V | PTE_U);
     }
 }
