6ef7b44bf68efed6d8dea56a6daaf53d3287659c
alex-kolovsky <kolovsky.dev@gmail.com>
2026-08-08 12:16:20 +0300

    feat(trap): implement trap context saving and routing

--- src/trap.rs
+++ src/trap.rs
@@ -1,5 +1,6 @@
 use crate::vcpu::Vcpu;
-
+use core::arch::naked_asm;
+use core::mem::offset_of;
 const SXLEN: u8 = 64;
 
 macro_rules! read_csr {
@@ -14,8 +15,94 @@ macro_rules! read_csr {
 
 #[unsafe(no_mangle)]
 #[unsafe(link_section = ".stvec")]
-extern "C" fn trap_handler() -> ! {
-    let scause = read_csr!("scause");
+#[unsafe(naked)]
+pub extern "C" fn trap_handler() {
+    naked_asm!(
+        // Swap a0 and sscratch.
+        "csrrw a0, sscratch, a0",
+
+        // a0 is now a pointer to a VCpu. Save registers except a0.
+        "sd ra, {ra_offset}(a0)",
+        "sd sp, {sp_offset}(a0)",
+        "sd gp, {gp_offset}(a0)",
+        "sd tp, {tp_offset}(a0)",
+        "sd t0, {t0_offset}(a0)",
+        "sd t1, {t1_offset}(a0)",
+        "sd t2, {t2_offset}(a0)",
+        "sd s0, {s0_offset}(a0)",
+        "sd s1, {s1_offset}(a0)",
+        "sd a1, {a1_offset}(a0)",
+        "sd a2, {a2_offset}(a0)",
+        "sd a3, {a3_offset}(a0)",
+        "sd a4, {a4_offset}(a0)",
+        "sd a5, {a5_offset}(a0)",
+        "sd a6, {a6_offset}(a0)",
+        "sd a7, {a7_offset}(a0)",
+        "sd s2, {s2_offset}(a0)",
+        "sd s3, {s3_offset}(a0)",
+        "sd s4, {s4_offset}(a0)",
+        "sd s5, {s5_offset}(a0)",
+        "sd s6, {s6_offset}(a0)",
+        "sd s7, {s7_offset}(a0)",
+        "sd s8, {s8_offset}(a0)",
+        "sd s9, {s9_offset}(a0)",
+        "sd s10, {s10_offset}(a0)",
+        "sd s11, {s11_offset}(a0)",
+        "sd t3, {t3_offset}(a0)",
+        "sd t4, {t4_offset}(a0)",
+        "sd t5, {t5_offset}(a0)",
+        "sd t6, {t6_offset}(a0)",
+
+        // Save scause
+        "csrr t0, scause",
+        "sd t0, {scause_offset}(a0)",
+        // Restore a0 from sscratch, and save in to VCpu.
+        "csrr t0, sscratch",
+        "sd t0, {a0_offset}(a0)",
+
+        // Switch to the hypervisor's stack.
+        "ld sp, {host_sp_offset}(a0)",
+
+        // a0 (first argument) is still the vcpu pointer here.
+        "call {handle_trap}",
+        handle_trap = sym handle_trap,
+        scause_offset = const offset_of!(Vcpu, scause),
+        host_sp_offset = const offset_of!(Vcpu, host_sp),
+        ra_offset = const offset_of!(Vcpu, ra),
+        sp_offset = const offset_of!(Vcpu, sp),
+        gp_offset = const offset_of!(Vcpu, gp),
+        tp_offset = const offset_of!(Vcpu, tp),
+        t0_offset = const offset_of!(Vcpu, t0),
+        t1_offset = const offset_of!(Vcpu, t1),
+        t2_offset = const offset_of!(Vcpu, t2),
+        s0_offset = const offset_of!(Vcpu, s0),
+        s1_offset = const offset_of!(Vcpu, s1),
+        a0_offset = const offset_of!(Vcpu, a0),
+        a1_offset = const offset_of!(Vcpu, a1),
+        a2_offset = const offset_of!(Vcpu, a2),
+        a3_offset = const offset_of!(Vcpu, a3),
+        a4_offset = const offset_of!(Vcpu, a4),
+        a5_offset = const offset_of!(Vcpu, a5),
+        a6_offset = const offset_of!(Vcpu, a6),
+        a7_offset = const offset_of!(Vcpu, a7),
+        s2_offset = const offset_of!(Vcpu, s2),
+        s3_offset = const offset_of!(Vcpu, s3),
+        s4_offset = const offset_of!(Vcpu, s4),
+        s5_offset = const offset_of!(Vcpu, s5),
+        s6_offset = const offset_of!(Vcpu, s6),
+        s7_offset = const offset_of!(Vcpu, s7),
+        s8_offset = const offset_of!(Vcpu, s8),
+        s9_offset = const offset_of!(Vcpu, s9),
+        s10_offset = const offset_of!(Vcpu, s10),
+        s11_offset = const offset_of!(Vcpu, s11),
+        t3_offset = const offset_of!(Vcpu, t3),
+        t4_offset = const offset_of!(Vcpu, t4),
+        t5_offset = const offset_of!(Vcpu, t5),
+        t6_offset = const offset_of!(Vcpu, t6),
+    );
+}
+pub fn handle_trap(vcpu: *mut Vcpu) {
+    let scause = unsafe { (*vcpu).scause };
 
     // Extract type of trap (exception/interrupt).
     let interrupt_bit = scause >> (SXLEN - 1);
@@ -28,7 +115,7 @@ extern "C" fn trap_handler() -> ! {
             9 => "Supervisor external interrupt",
             13 => "Counter-overflow interrupt",
 
-            _ => "Unhandable exeption code",
+            _ => "Unhandable exception code",
         }
     } else {
         match exception_code {
@@ -40,17 +127,23 @@ extern "C" fn trap_handler() -> ! {
             5 => "Load access fault",
             6 => "Store/AMO address misaligned",
             7 => "Store/AMO access fault",
-            8 => "Environment call from U-mode",
-            9 => "Environment call from S-mode",
+            8 => "Environment call from U-mode or VU-mode",
+            9 => "Environment call from HS-mode",
+            10 => "Environment call from VS-mode",
+            11 => "Environment call from M-mode",
             12 => "Instruction page fault",
             13 => "Load page fault",
             15 => "Store/AMO page fault",
+            16 => "Double trap",
             18 => "Software check",
             19 => "Hardware error",
+            20 => "Instruction guest-page fault",
+            21 => "Load guest-page fault",
+            22 => "Virtual instruction",
+            23 => "Store/AMO guest-page fault",
 
-            _ => "Unhandable exeption code",
+            _ => "Unhandable exception code",
         }
     };
-
     panic!("A trap occurred ({})", scause_str);
 }
