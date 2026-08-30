0d649a967530db28385ec7e69478debbfe093393
alex-kolovsky <alex@kolovsky.me>
2026-08-30 18:23:54 +0300

    refactor(guest): move guests initialization into a separate function

--- src/guest.rs
+++ src/guest.rs
@@ -101,3 +101,54 @@ pub fn claim_assigned_hart_slot_if_available(guest_id: usize) -> Result<(), ()> 
 
     if is_free.is_ok() { Ok(()) } else { Err(()) }
 }
+
+pub fn initialize_guests() {
+    // Fill the guests with VCPUs.
+    for guest in GUESTS.iter() {
+        let vcpus: *mut [Option<Vcpu>; MAX_SUPPORTED_HARTS_PER_GUEST] = guest.vcpus.get();
+
+        for i in 0..(guest.harts_cap + guest.assigned_harts_cap) {
+            // Get the pointer to the first vCPU in guest.vcpus.
+            let base_ptr: *mut Option<Vcpu> = vcpus as *mut Option<Vcpu>;
+
+            // Get the vcpu pointer by its ID.
+            let cur_vcpu: *mut Option<Vcpu> = unsafe { base_ptr.add(i) };
+
+            unsafe {
+                if let Some(vcpu_ref) = &mut *cur_vcpu {
+                    let vcpu: *mut Vcpu = vcpu_ref as *mut Vcpu;
+
+                    // Load the allocated vcpu instead of an empty slot into guest.vcpus[i].
+                    *vcpu = allocate_guest_memory(guest.entry_gpa, guest.data, i as u64);
+
+                    // Load this vcpu pointer into guest.vcpu_ptrs[i].
+                    let mut vcpu_ptr = guest.vcpu_ptrs.lock();
+                    (*vcpu_ptr)[i] = Some(vcpu);
+                } else {
+                    unreachable!();
+                }
+            }
+        }
+
+        // Initialize assigned hart pointers before booting the secondary cores.
+        for assigned_hart_id in guest.assigned_harts.iter().flatten() {
+            let assigned_guests = crate::HARTS[*assigned_hart_id as usize]
+                .assigned_guests
+                .get();
+
+            let mut vcpu_ptrs = guest.vcpu_ptrs.lock();
+            unsafe {
+                for id in 0..(*assigned_guests).len() {
+                    if (*assigned_guests)[id].is_null() {
+                        // First values of the vcpu_ptrs array are intended for assigned harts.
+                        (*assigned_guests)[id] = (*vcpu_ptrs)[0].unwrap();
+
+                        // Remove the vcpu pointer of a assigned hart from the vcpu_ptrs array.
+                        (*vcpu_ptrs)[0] = None;
+                        (*vcpu_ptrs).rotate_left(1);
+                    }
+                }
+            }
+        }
+    }
+}
